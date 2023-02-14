b2bc8a518823eb647e235ff6bea09225ab3521ab
Grandman7777 <kokaiya7777@mail.com>
2023-02-14 18:33:55 +0200

    Get count task resolve

---
+++ controllers/Task.controller.js
@@ -0,0 +1,34 @@
+ const {Task, User} = require('./models');
+
+module.exports.createTask = async (req, res, next) => {
+    try {
+        const {body, params: {userId}} = req;
+        const user = User.findByPk(userId);
+        const result = await user.createTask(body);
+        return res.status(201).send(result);
+    } catch (error) {
+        next (error);
+    }
+}
+
+module.exports.getAllUserTasks = async (req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        const user = User.findByPk(userId);
+        const tasks = await user.getTasks();
+        return res.status(200).send(tasks);
+    } catch (error) {
+        next (error);
+    }
+}
+
+module.exports.getCountofTasks = async (req, res, next) => {
+    try {
+        const {params: {userId}} = req;
+        const user = User.findByPk(userId);
+        const tasks = await user.countTasks();
+        return res.status(200).send(`${tasks}`);
+    } catch (error) {
+        next(error);
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const UserController = require('../controllers/User.controller');
+const TaskController = require('../controllers/Task.controller')
 
 const router = Router();
 
@@ -8,5 +9,9 @@ router.get('/user', UserController.findAll);
 router.get('/user/:id', UserController.findOnePk);
 router.delete('/user/:id', UserController.deleteByPk);
 router.put('/user/:id', UserController.updateUser);
+
+router.post('/task/userid', TaskController.createTask);
+router.get('/task/:userid', TaskController.getAllUserTasks);
+router.get('./task-count/:userId', TaskController.getCountofTasks);
 
 module.exports = router;
